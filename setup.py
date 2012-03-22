1f95541366bcab96002fc614e0022cd18e9bfaef
Jeff Lindsay <progrium@gmail.com>
2012-03-22 03:50:45 -0700

    bumping version, primarily to test persistence on heroku

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='requestbin',
-    version='1.0.0',
+    version='1.1.0',
     author='Jeff Lindsay',
     author_email='jeff.lindsay@twilio.com',
     description='HTTP request collector and inspector',
