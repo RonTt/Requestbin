afb0eadab3545ed715562038396a5170616faafe
John Sheehan <johnsheehan@gmail.com>
2013-04-26 10:03:19 -0700

    add additional HTTP method support

--- requestbin/web.py
+++ requestbin/web.py
@@ -5,7 +5,7 @@ app.debug = True
 app.secret_key = '((Z O*T^@YX R~XHH!jesrgwerg]LWerg@#R#$TT'
 
 app.add_url_rule('/', 'views.home')
-app.add_url_rule('/<name>', 'views.bin', methods=['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS', 'HEAD'])
+app.add_url_rule('/<name>', 'views.bin', methods=['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS', 'HEAD', 'PATCH', 'TRACE'])
 
 app.add_url_rule('/docs/<name>', 'views.docs')
 app.add_url_rule('/api/v1/bins', 'api.bins', methods=['POST'])
