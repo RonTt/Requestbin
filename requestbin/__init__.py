4ee3fbb6095d5f32cdf2132bc528073a855e0b07
Aki Tuomi <cmouse@cmouse.fi>
2016-09-13 10:11:09 +0300

    Allow any URL after bin ID.
    This lets you post in with binid/path, binid.suffix/path,
    binid.suffix or binid.

--- requestbin/__init__.py
+++ requestbin/__init__.py
@@ -63,7 +63,7 @@ app.jinja_env.filters['exact_time'] = exact_time
 app.jinja_env.filters['short_date'] = short_date
 
 app.add_url_rule('/', 'views.home')
-app.add_url_rule('/<name>', 'views.bin', methods=['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS', 'HEAD', 'PATCH', 'TRACE'])
+app.add_url_rule('/<path:name>', 'views.bin', methods=['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS', 'HEAD', 'PATCH', 'TRACE'])
 
 app.add_url_rule('/docs/<name>', 'views.docs')
 app.add_url_rule('/api/v1/bins', 'api.bins', methods=['POST'])
